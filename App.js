23d7a23d126b6ccec5e2210859a039ff7b1c3e62
Gayatri Vedpathak <99533431+gayatrivedpathak@users.noreply.github.com>
2023-03-29 19:13:57 +0530

    Implement the 50 times call

--- App.js
+++ App.js
@@ -1,19 +1,37 @@
 import React, {useEffect, useState} from 'react';
 
-import {Button, TextInput, View} from 'react-native';
+import {Button, Text, TextInput, View} from 'react-native';
 import {NativeModules} from 'react-native';
 import generateText from './TextGenerator';
 const StringFormat = NativeModules.StringFormat;
 
+// const calculatePercentile = ([...data]) => {
+//   const count = data.length;
+// };
+
 const App = () => {
   const [text, setText] = useState('Enter Text');
+  const [responseTime, setResponseTime] = useState('');
+  const responcesTimeTaken = [];
 
   useEffect(() => {
     setText(generateText(50000));
   }, []);
 
   const capitaliseText = async () => {
-    const foo = await StringFormat.capitalise(text);
+    let foo;
+    // const startTime = new Date().getTime();
+    for (let i = 0; i < 50; i++) {
+      const startTime = new Date().getTime();
+      foo = await StringFormat.capitalise(text); // is the understanding correct ?
+      const endTime = new Date().getTime();
+      responcesTimeTaken.push(endTime - startTime);
+    }
+    const endTime = new Date().getTime();
+    console.log(responcesTimeTaken.length);
+    setResponseTime(responcesTimeTaken.toString());
+
+    console.log(responseTime);
     return foo;
   };
   return (
@@ -23,6 +41,10 @@ const App = () => {
         title="Capitalise"
         onPress={async () => setText(await capitaliseText())}
       />
+      <View>
+        <Text>Time Taken</Text>
+        <Text>{responseTime} ms</Text>
+      </View>
     </View>
   );
 };
